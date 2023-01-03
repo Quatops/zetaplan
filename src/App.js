1fa8fe1e7650233687e9d8d10ab5fecd37270e9e
byeong <abm1640@naver.com>
2023-01-03 10:20:10 +0900

    feat : project setting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 function App() {
-  return <div className='App'></div>;
+  return <div className='App'>시자가다</div>;
 }
 
 export default App;
---
+++ src/pages/Main/Page1/Page1.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import style  from './Page1.module.css';
+
+export default function Page1() {
+  return (
+    <div>Page1</div>
+  )
+}
