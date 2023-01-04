f1d88fa5e8429b45d2f3d08ce814301f34ff8779
byeong <abm1640@naver.com>
2023-01-04 11:17:40 +0900

    feat : add  pull-page scroll nav active-mode css, setting layout mainpage

--- src/pages/Main/Page1/index.jsx
+++ src/pages/Main/Page1/index.jsx
@@ -4,7 +4,20 @@ import styles from "./styles.module.css";
 export default function Page1() {
   return (
     <div className={styles.page_wrapper}>
-      <div className={styles.main_container}></div>
+      <div className={styles.main_container}>
+        <div className={styles.left}>
+          <div className={styles.banner}></div>
+          <div className={styles.main_intro}></div>
+        </div>
+        <div className={styles.right}>
+          <div className={styles.call_info}></div>
+          <div className={styles.infos}>
+            <div className={styles.company_info}></div>
+            <div className={styles.ect_info}></div>
+            <div className={styles.map_info}></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Main/ScrollMenu/index.jsx
+++ src/pages/Main/ScrollMenu/index.jsx
@@ -4,8 +4,9 @@ import React from "react";
 const Menu = ({ num, pageIdx, pageName }) => {
   return (
     <div className={`${styles.scroll} ${pageIdx === num && styles.active}`}>
-      <div className={`${styles.name} ${pageIdx === num && styles.line}`}>
+      <div className={styles.name}>
         {pageName}
+        {pageIdx === num && <div className={styles.line}></div>}
       </div>
     </div>
   );
@@ -15,9 +16,9 @@ const ScrollMenu = ({ pageIdx }) => {
   return (
     <div className={styles.menu_wrapper}>
       <div className={styles.menu_list}>
-        <Menu num={1} scrollIdx={pageIdx} pageName="MAIN"></Menu>
-        <Menu num={2} scrollIdx={pageIdx} pageName="DOMESTIC"></Menu>
-        <Menu num={3} scrollIdx={pageIdx} pageName="GLOBAL"></Menu>
+        <Menu num={1} pageIdx={pageIdx} pageName="MAIN"></Menu>
+        <Menu num={2} pageIdx={pageIdx} pageName="DOMESTIC"></Menu>
+        <Menu num={3} pageIdx={pageIdx} pageName="GLOBAL"></Menu>
       </div>
     </div>
   );
