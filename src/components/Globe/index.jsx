0f24d3a2715c7d3751dfbdc09c260c7f16d79a6f
byeong <abm1640@naver.com>
2023-01-09 18:00:04 +0900

    fix : #21 나라이동 로직 수정

--- src/components/Globe/index.jsx
+++ src/components/Globe/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import ReactGlobe from "react-globe.gl";
 import Earth from "assets/earth_texture.jpg";
 import "tippy.js/dist/tippy.css";
@@ -6,57 +6,93 @@ import "tippy.js/animations/scale.css";
 const markers = [
   {
     id: 0,
-    city: "Korea",
-    color: "blue",
-    size: 7 + Math.random() * 40,
-    lat: 37.541,
-    lng: 126.986,
+    city: "France",
+    color: "#d7000e",
+    size: 10 + Math.random() * 30,
+    lat: 46.227638,
+    lng: 2.213749,
     value: 50,
   },
   {
     id: 1,
-    city: "Singapore",
+    city: "United Kingdom",
     color: "#d7000e",
-    size: 7 + Math.random() * 30,
-    lat: 1.3521,
-    lng: 103.8198,
+    size: 10 + Math.random() * 30,
+    lat: 55.378051,
+    lng: -3.435973,
     value: 25,
   },
   {
     id: 2,
-    city: "New York",
+    city: "Spain",
     color: "#d7000e",
-    size: 7 + Math.random() * 30,
-    lat: 40.73061,
-    lng: -73.935242,
+    size: 10 + Math.random() * 30,
+    lat: 40.463667,
+    lng: -3.74922,
     value: 35,
   },
   {
     id: 3,
-    city: "San Francisco",
+    city: "Germany",
     color: "#d7000e",
-    size: 7 + Math.random() * 30,
-    lat: 37.773972,
-    lng: -122.431297,
+    size: 10 + Math.random() * 30,
+    lat: 51.165691,
+    lng: 10.451526,
     value: 135,
   },
   {
     id: 4,
-    city: "London",
+    city: "China",
     color: "#d7000e",
-    size: 7 + Math.random() * 30,
-    lat: 51.5074,
-    lng: 0.1278,
+    size: 10 + Math.random() * 30,
+    lat: 35.86166,
+    lng: 104.195397,
     value: 80,
   },
   {
     id: 5,
-    city: "Los Angeles",
+    city: "Vietnam",
     color: "#d7000e",
-    size: 7 + Math.random() * 30,
-    lat: 29.7604,
-    lng: -95.3698,
+    size: 15 + Math.random() * 30,
+    lat: 14.058324,
+    lng: 108.277199,
     value: 54,
+  },
+  {
+    id: 6,
+    city: "Singapore",
+    color: "blue",
+    size: 15 + Math.random() * 30,
+    lat: 1.352083,
+    lng: 103.819836,
+    value: 50,
+  },
+  {
+    id: 7,
+    city: "Malaysia",
+    color: "blue",
+    size: 15 + Math.random() * 30,
+    lat: 4.210484,
+    lng: 101.975766,
+    value: 50,
+  },
+  {
+    id: 8,
+    city: "Japan",
+    color: "blue",
+    size: 15 + Math.random() * 30,
+    lat: 36.204824,
+    lng: 138.252924,
+    value: 50,
+  },
+  {
+    id: 9,
+    city: "Indonesia",
+    color: "blue",
+    size: 15 + Math.random() * 30,
+    lat: -0.789275,
+    lng: 113.921327,
+    value: 50,
   },
 ];
 export default function Globe({ activeIdx, updateActiveIdx }) {
@@ -90,7 +126,6 @@ export default function Globe({ activeIdx, updateActiveIdx }) {
 </svg>`;
   //currentColor
 
-  const shiftAmmount = 0.2 * window.innerWidth;
   return (
     <div
       style={{ width: "100%", height: "650px", overflow: "hidden" }}
--- src/pages/MainPage/GlobalNetwork/index.jsx
+++ src/pages/MainPage/GlobalNetwork/index.jsx
@@ -8,15 +8,18 @@ import Globe from "components/Globe";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 import NetworkCard from "components/NetworkdCard";
 
+import { nations } from "constants/nations";
+
 export default function GlobalNetwork() {
   const [activeContinentIdx, setActiveContinentIdx] = useState(0);
   const [activeNationIdx, setActiveNationIdx] = useState(0);
   const updateActiveNationIdx = (idx) => {
     setActiveNationIdx(idx);
+    setActiveContinentIdx(nations[idx].continent);
+    console.log(nations[idx].continent);
   };
   const updateActiveContinentIdx = (idx) => {
     setActiveContinentIdx(idx);
-    setActiveNationIdx(0);
   };
   const contimentBtns = () => {
     const result = [];
@@ -36,119 +39,7 @@ export default function GlobalNetwork() {
     }
     return result;
   };
-  const nations_name = [
-    ["프랑스", "영국", "유럽3", "유럽4"],
-    ["중국", "베트남", "싱가포르", "밀레이시아", "일본", "인니"],
-    ["아프리카1", "아프리카2", "아프리카3", "아프리카4"],
-  ];
 
-  const nations = [
-    {
-      id: 0,
-      "nation-eng": "CHINA",
-      "nation-kor": "중국",
-      networks: [
-        {
-          name: "ICI",
-          detail: "Beijing B&R International Co-incubation",
-          intro_link: "",
-          logo: "china1",
-        },
-        {
-          name: "베이징기술교역촉진중심",
-          detail: "Beijing Technology Transfer & Exchange,dasdass dasddasdas",
-          intro_link: "",
-          logo: "china2",
-        },
-        {
-          name: "상하이기술교역소",
-          detail: "Shanghai Technology Transfer & Exchange Center",
-          intro_link: "",
-          logo: "china3",
-        },
-        {
-          name: "절강성과학기술교류중심",
-          detail: "Zhejang Provincial and Technology Center...",
-          intro_link: "",
-          logo: "china5",
-        },
-        {
-          name: "절강성과학기술교류중심",
-          detail: "Zhejang Provincial and Technology Center...",
-          intro_link: "",
-          logo: "china5",
-        },
-        {
-          name: "절강성과학기술교류중심",
-          detail: "Zhejang Provincial and Technology Center...",
-          intro_link: "",
-          logo: "china5",
-        },
-      ],
-    },
-    {
-      id: 1,
-      "nation-eng": "Vietnam",
-      "nation-kor": "베트남",
-      networks: [
-        {
-          name: "ICI",
-          detail: "Beijing B&R International Co-incubation",
-          "intro-link": "",
-          logo: "china1",
-        },
-        {
-          name: "베이징기술교역촉진중심",
-          detail: "Beijing Technology Transfer & Exchange, ...",
-          "intro-link": "",
-          logo: "china2",
-        },
-        {
-          name: "상하이기술교역소",
-          detail: "Shanghai Technology Transfer & Exchange Center",
-          "intro-link": "",
-          logo: "china3",
-        },
-        {
-          name: "절강성과학기술교류중심",
-          detail: "Zhejang Provincial and Technology Center...",
-          "intro-link": "",
-          logo: "china4",
-        },
-      ],
-    },
-    {
-      id: 2,
-      "nation-eng": "New York",
-      "nation-kor": "뉴욕",
-      networks: [
-        {
-          name: "ICI",
-          detail: "Beijing B&R International Co-incubation",
-          "intro-link": "",
-          logo: "china1",
-        },
-        {
-          name: "베이징기술교역촉진중심",
-          detail: "Beijing Technology Transfer & Exchange, ...",
-          "intro-link": "",
-          logo: "china2",
-        },
-        {
-          name: "상하이기술교역소",
-          detail: "Shanghai Technology Transfer & Exchange Center",
-          "intro-link": "",
-          logo: "china3",
-        },
-        {
-          name: "절강성과학기술교류중심",
-          detail: "Zhejang Provincial and Technology Center...",
-          "intro-link": "",
-          logo: "china4",
-        },
-      ],
-    },
-  ];
   return (
     <div className={styles.page_wrapper}>
       <div className={`${styles.content_wrap}`}>
@@ -160,44 +51,55 @@ export default function GlobalNetwork() {
           <div className={styles.nations_list_wrap}>
             <header className={styles.continents}>{contimentBtns()}</header>
             <div className={styles.nations_wrap}>
-              {nations_name[activeContinentIdx].map((nation, index) => (
-                <p
-                  className={`${styles.nation} ${
-                    activeNationIdx === index && styles.active
-                  }`}
-                  onClick={() => setActiveNationIdx(index)}
-                >
-                  {nation}
-                </p>
-              ))}
+              {nations.map(
+                (nation, index) =>
+                  nation.continent === activeContinentIdx && (
+                    <p
+                      key={nations.id}
+                      className={`${styles.nation} ${
+                        activeNationIdx === index && styles.active
+                      }`}
+                      onClick={() => setActiveNationIdx(index)}
+                    >
+                      {nation.name_kor}
+                    </p>
+                  )
+              )}
             </div>
           </div>
         </article>
+        {/* 경계선 그리기 */}
         <div className={styles.boundary}></div>
         <article className={styles.nations_networklist_wrap}>
           <header className={styles.nations_header_wrap}>
-            <p
-              className={styles.arrow_icon}
-              onClick={() =>
-                setActiveNationIdx((prev) => prev !== 0 && prev - 1)
-              }
-            >
-              <FiChevronLeft />
-            </p>
+            {activeNationIdx !== 0 ? (
+              <p
+                className={styles.arrow_icon}
+                onClick={() => updateActiveNationIdx(activeNationIdx - 1)}
+              >
+                <FiChevronLeft />
+              </p>
+            ) : (
+              <div></div>
+            )}
             <div className={styles.nations_title_wrap}>
               <div className={styles.nations_title_eng}>
-                {nations[activeNationIdx]["nation-eng"]} NETWORK
+                {nations[activeNationIdx].name_eng} NETWORK
               </div>
               <div className={styles.nations_title_kor}>
-                {nations[activeNationIdx]["nation-kor"]}
+                {nations[activeNationIdx].name_kor}
               </div>
             </div>
-            <p
-              className={styles.arrow_icon}
-              onClick={() => setActiveNationIdx((prev) => prev + 1)}
-            >
-              <FiChevronRight />
-            </p>
+            {activeNationIdx !== nations.length - 1 ? (
+              <p
+                className={styles.arrow_icon}
+                onClick={() => updateActiveNationIdx(activeNationIdx + 1)}
+              >
+                <FiChevronRight />
+              </p>
+            ) : (
+              <div></div>
+            )}
           </header>
           <article className={styles.network_list}>
             {nations[activeNationIdx].networks.map((network, index) => (
