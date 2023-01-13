dafec878d5c0508ec0c192ec1e2e4c443e0d46b8
byeong <abm1640@naver.com>
2023-01-13 11:10:25 +0900

    feat : #33 excel -> json 변환, lazy()로 성능 향상

--- src/pages/MainPage/InvestmentPortfolio/index.jsx
+++ src/pages/MainPage/InvestmentPortfolio/index.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import styles from './styles.module.css';
+import portfolio from 'constants/portfolio.json';
 
-const sectorBtn = (name) => {
-  return <button className={styles.sector_btn}>{name}</button>;
-};
 const sectors = [
   'ALL',
   '소재부품장비',
@@ -27,23 +25,8 @@ const sectors = [
   '패션',
 ];
 
-const portfolios = [
-  {
-    logo: '',
-    from: '제타법인',
-    name: '소재의 맥',
-    intro: '문서 출력물 보안 솔루션',
-    tags: ['소재부품장비', '보안'],
-  },
-  {
-    logo: '',
-    from: '제타법인',
-    name: '소재의 맥',
-    intro: '문서 출력물 보안 솔루션',
-    tags: ['소재부품장비', '보안'],
-  },
-];
 export default function InvestmentPortfolio() {
+  console.log(portfolio);
   return (
     <div className={styles.page_wrapper}>
       <div className={styles.contents_wrapper}>
@@ -57,7 +40,9 @@ export default function InvestmentPortfolio() {
         <section className={styles.sector}>
           <p>sector</p>
           {sectors.map((value, index) => (
-            <>{sectorBtn(value)}</>
+            <button key={index} className={styles.sector_btn}>
+              {value}
+            </button>
           ))}
         </section>
         <section className={styles.content}></section>
