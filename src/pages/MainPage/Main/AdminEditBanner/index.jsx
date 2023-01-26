6893ca96ea2c2c1e1960f4444d99286efcd210a3
Sukong <sukong0507@gmail.com>
2023-01-26 22:09:37 +0900

    feat : #59 footer 스크롤 이동까지 구현

---
+++ src/pages/MainPage/Main/AdminEditBanner/index.jsx
@@ -0,0 +1,60 @@
+import AdminEditContainer from 'components/AdminEditContainer';
+import React, { useState } from 'react';
+import styles from './styles.module.css';
+import Button from 'components/SubmitButton';
+import { FaTrashAlt } from 'react-icons/fa';
+
+export default function AdminEditBanner({ handleEditSubmit, baseImages }) {
+  const [images, setImages] = useState(baseImages);
+
+  const handleChange = (e) => {
+    const { files } = e.target;
+    const nowImageURLList = [...images];
+    for (let i = 0; i < files.length; ++i) {
+      const nowImageUrl = URL.createObjectURL(files[i]);
+      nowImageURLList.push(nowImageUrl);
+    }
+    if (nowImageURLList.length > 10) {
+      nowImageURLList = nowImageURLList.slice(0, 10);
+    }
+    setImages(nowImageURLList);
+  };
+  const handleDeleteImage = (id) => {
+    setImages(images.filter((_, index) => index !== id));
+  };
+  return (
+    <AdminEditContainer buttonHeight="400px" title="배너이미지 수정">
+      <div className={styles.images_wrap}>
+        {images.map((images, index) => (
+          <div className={styles.image} key={index}>
+            <img src={images} alt="배너이미지" />
+            <p
+              className={styles.delete_icon}
+              onClick={() => handleDeleteImage(index)}>
+              <FaTrashAlt />
+            </p>
+          </div>
+        ))}
+      </div>
+      <form
+        autoComplete="off"
+        className={styles.form_wrap}
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleEditSubmit(e);
+        }}>
+        <input
+          type="file"
+          accept="image/*"
+          multiple
+          name="file"
+          onChange={handleChange}
+        />
+
+        <Button widthSize="100%" handleSubmit={handleEditSubmit}>
+          변경하기
+        </Button>
+      </form>
+    </AdminEditContainer>
+  );
+}
