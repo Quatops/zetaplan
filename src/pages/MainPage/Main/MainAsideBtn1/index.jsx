e2cae8406ed54aee6114d351874292d5db5bad0b
Sukong <sukong0507@gmail.com>
2023-02-04 19:12:26 +0900

    fix : #77 오류 수정

--- src/pages/MainPage/Main/MainAsideBtn1/index.jsx
+++ src/pages/MainPage/Main/MainAsideBtn1/index.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 import styles from './styles.module.css';
 import { useNavigate } from 'react-router-dom';
 
-export default function MainAsideBtn1({ aside_btn1 }) {
+export default function MainAsideBtn1({ aside_btn1, subCategory, category }) {
   const navigate = useNavigate();
-
   return (
     <>
       {aside_btn1.map((value, index) => (
         <button
           className={styles.ass_btn}
           key={index}
-          onClick={() => navigate('/')}>
+          onClick={() => navigate(subCategory[category[6 + index].id][0].path)}>
           <div className={styles.ass_icon}>
             <img src={value.src} alt="icon" />
           </div>
